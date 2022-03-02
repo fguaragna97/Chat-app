767ce83b52ed06f4e6391b367e3da5b829c17406
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-03-02 13:12:01 -0800

    Connect the backend with the frontend
    Connected the backend with the frontend using the socket.io

--- server/index.js
+++ server/index.js
@@ -5,8 +5,11 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 
 app.use(cors());
+
+// creating the server
 const server = http.createServer(app);
 
+// creating a new instance of server and using cors to prevent issues
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -14,6 +17,21 @@ const io = new Server(server, {
   },
 });
 
+// using socket io to listen for events when a user connects and disconnects
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  // listen for the event join room
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID:${socket.id} joined room: ${data}`);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+// create the server where our app is running locally
 server.listen(3001, () => {
   console.log("SERVER RUNNING");
 });
