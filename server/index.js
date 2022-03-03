cb8201b94ace10da50b98c676983444dab0c1697
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-03-03 12:50:31 -0800

    .

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // creating a new instance of server and using cors to prevent issues
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
